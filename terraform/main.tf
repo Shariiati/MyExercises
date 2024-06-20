b4cb69e0cc1075a00111d78ef4d352be911a77ad
Hossein Shariati <46321565+Shariiati@users.noreply.github.com>
2024-06-21 00:20:43 +0330

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@
 #### resources ####
 ## https://developer.hashicorp.com/terraform (Automate Infrastructure on Any Cloud)
 ## https://developer.hashicorp.com/terraform/language (Terraform Language Documentation)
+##
 ###    ESXI    ###
 ## https://github.com/josenk/terraform-provider-esxi
 ## https://www.chunho-ling.com/terraform-create-virtual-machines-in-esxi-server/ (Create Virtual Machines in ESXi Server)
@@ -20,7 +21,7 @@
 ## https://registry.terraform.io/providers/gitlabhq/gitlab/latest/docs
 ## https://developer.hashicorp.com/terraform/tutorials/modules/module (Modules overview)
 ## https://developer.hashicorp.com/terraform/language/style (Style Guide)
-
+####
 
 resource "esxi_guest" "iac-mng" {
   guest_name     = var.guest_name
@@ -32,7 +33,7 @@ resource "esxi_guest" "iac-mng" {
   guestos        = var.guestos
   virthwver      = var.virthwver
   power          = var.power
-  # clone_from_vm      = "ConfigurationManager - 79.132.192.9"
+  # clone_from_vm      = "ConfigurationManager - x.x.x.x"
 
   network_interfaces {
     virtual_network = var.virtual_network_1
@@ -46,7 +47,7 @@ resource "esxi_guest" "iac-mng" {
 ##
 
 #resource "esxi_guest" "prd" {
-# guest_name         = "prd (tmp) - 79.132.192.6"
+# guest_name         = "tmp - x.x.x.x"
 # numvcpus           = "4"
 # boot_disk_size     = "150"
 # memsize            = "8192"
@@ -61,11 +62,11 @@ resource "esxi_guest" "iac-mng" {
 #  network_interfaces {
 #    virtual_network = var.virtual_network_1
 #    nic_type        = "vmxnet3"
-#    mac_address     = "fe80::20c:29ff:fe4f:9147"
+#    mac_address     = "xxxx::xxx:xxxx:xxx:xxxx"
 #}
 #  network_interfaces {
 #    virtual_network = var.virtual_network_2
 #    nic_type        = "vmxnet3"
-#    mac_address     = "fe80::20c:29ff:fe4f:9151"
+#    mac_address     = "xxxx::xxx:xxxx:xxxx:xxxx"
 #  }
 #}
